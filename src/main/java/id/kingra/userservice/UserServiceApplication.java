de15ec59636b1cacd836c8f8df845c6bff5b4733
riskigalangsaputra <riskigalangsaputra@gmail.com>
2024-05-24 20:06:22 +0700

    update configuration

--- src/main/java/id/kingra/userservice/UserServiceApplication.java
+++ src/main/java/id/kingra/userservice/UserServiceApplication.java
@@ -1,14 +1,9 @@
 package id.kingra.userservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.cloud.openfeign.FeignAutoConfiguration;
 
 @SpringBootApplication
-//@EnableFeignClients("id.kingra.userservice.feignclient")
-//@ImportAutoConfiguration({FeignAutoConfiguration.class})
 public class UserServiceApplication {
 
 	public static void main(String[] args) {
